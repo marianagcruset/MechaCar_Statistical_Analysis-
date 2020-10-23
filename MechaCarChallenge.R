6132655f10840d83d58963097040e5901d1a8f7b
marianagcruset <marianagcruset@gmail.com>
2020-10-23 16:29:40 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,6 @@ suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAs
 # Summarize PSI column
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) 
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Deliverable 3
+t.test(suspension_coil$PSI, mu=1500)
